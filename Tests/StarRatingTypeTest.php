d0e612703c3ac7df0eb7e5c6284a024179cceaed
nurikabe <nurikabe@yahoo.com>
2014-10-11 11:47:54 -0400

    Fix namespace

--- Tests/StarRatingTypeTest.php
+++ Tests/StarRatingTypeTest.php
@@ -2,14 +2,13 @@
 
 namespace CCC\StarRatingBundle\Tests;
 
-use Symfony\Component\Form\AbstractType;
-use CCC\StarRatingBundle\Form\StarRatingType;
+use CCC\StarRatingBundle\Form\Type\StarRatingType;
 
 class StarRatingTypeTest extends \PHPUnit_Framework_TestCase
 {
     public function testAncestry()
     {
         $type = new StarRatingType();
-	$this->assertEquals('choice', $type->getParent());
+        $this->assertEquals('choice', $type->getParent());
     }
 }
