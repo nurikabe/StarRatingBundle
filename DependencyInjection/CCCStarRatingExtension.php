0ff0edc958adc1bae4e78002842c3c20626c3e14
nurikabe <nurikabe@yahoo.com>
2014-10-11 11:19:06 -0400

    Remove dead code.  Move form type into Type directory.

--- DependencyInjection/CCCStarRatingExtension.php
+++ DependencyInjection/CCCStarRatingExtension.php
@@ -20,8 +20,6 @@ class CCCStarRatingExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         // No config yet
-        //$configuration = new Configuration();
-        //$config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
