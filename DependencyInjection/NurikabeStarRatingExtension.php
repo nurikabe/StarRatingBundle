bf7bc25ddcd0e5a41294d0471f190795d2192949
Evan Owens <evan.owens@tamago-db.com>
2014-12-31 14:43:41 -0500

    Update namespace to reflect maintainer

--- DependencyInjection/NurikabeStarRatingExtension.php
+++ DependencyInjection/NurikabeStarRatingExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CCC\StarRatingBundle\DependencyInjection;
+namespace Nurikabe\StarRatingBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class CCCStarRatingExtension extends Extension
+class NurikabeStarRatingExtension extends Extension
 {
     /**
      * {@inheritDoc}
